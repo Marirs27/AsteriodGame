d8e38aa250f11366d603bc18dfc87fa223c6761d
Marirs27 <68460929+Marirs27@users.noreply.github.com>
2020-08-06 15:30:44 +0530

    ProjectAsteroid

---
+++ Assets/Scripts/ShipThrust.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipThrust : MonoBehaviour
+{
+    // Fields required
+    Rigidbody2D Ship;
+    Vector2 ThrustDirection = new Vector2(1, 0);
+    const float ThrustForce = 10;
+    float Radius;
+    const float RotateDegreesPerSecond = 90 ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ship = gameObject.GetComponent<Rigidbody2D>();
+        Radius = gameObject.GetComponent<CircleCollider2D>().radius;
+    }
+
+    /// <summary>
+    /// Update is called every Frame
+    /// </summary>
+    private void Update()
+    {
+        float RotateInput = Input.GetAxis("Rotate");
+        if (RotateInput != 0)
+        {
+            float TotalDegreesToBeRotated = RotateDegreesPerSecond * Time.deltaTime;
+            if (RotateInput < 0)
+                TotalDegreesToBeRotated *= -1;
+            transform.Rotate(Vector3.forward, TotalDegreesToBeRotated);
+            float AngleInRadians = transform.eulerAngles.z * Mathf.Deg2Rad;
+            ThrustDirection = new Vector2(Mathf.Cos(AngleInRadians), Mathf.Sin(AngleInRadians));
+        }
+    }
+
+    /// <summary>
+    /// Fixed Update Called Every Second
+    /// </summary>
+    public void FixedUpdate()
+    {
+        if (Input.GetAxis("Thrust") > 0)
+        {
+            Ship.AddForce(ThrustDirection * ThrustForce);
+        }
+    }
+
+    /// <summary>
+    /// OnBecameInvisible is called when the associated game object is no longer visible by any camera.
+    /// </summary>
+    void OnBecameInvisible()
+    {
+        Vector2 Position = transform.position;
+        if (Position.x > ScreenUtils.ScreenRight)
+            Position.x = ScreenUtils.ScreenLeft;
+        else if (Position.x < ScreenUtils.ScreenLeft)
+            Position.x = ScreenUtils.ScreenRight;
+        if (Position.y > ScreenUtils.ScreenTop)
+            Position.y = ScreenUtils.ScreenBottom;
+        else if (Position.y < ScreenUtils.ScreenBottom)
+            Position.y = ScreenUtils.ScreenTop;
+        transform.position = Position;
+    }
+}
---
+++ Assets/Scripts/ShowDamage.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ShowDamage : MonoBehaviour
+{
+    // Serialized Fields to show damage is done to the Ship
+    [SerializeField]
+    GameObject prefabShip;
+
+    [SerializeField]
+    Sprite SpriteNumber01;
+
+    [SerializeField]
+    Sprite SpriteNumber02;
+
+    [SerializeField]
+    Sprite SpriteNumber03;
+
+    const float TimeFactor = 0.2f;
+    float elapsedTime = 0;
+    bool InputGiven = false;
+    bool DangerDone = true;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        elapsedTime += Time.deltaTime;
+        if (DangerDone)
+        {
+            if (Input.GetAxis("Damage") > 0)
+            {
+                DangerDone = false;
+                InputGiven = true;
+            }
+        }
+        if (InputGiven)
+        {
+            Danger();
+        }
+    }
+
+    void Danger()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (elapsedTime > 0 * TimeFactor && elapsedTime < 2 * TimeFactor)
+        {
+            spriteRenderer.sprite = SpriteNumber01;
+        }
+        else if (elapsedTime > 2 * TimeFactor && elapsedTime < 4 * TimeFactor)
+        {
+            spriteRenderer.sprite = SpriteNumber02;
+        }
+        else if (elapsedTime > 4 * TimeFactor && elapsedTime < 6 * TimeFactor)
+        {
+            spriteRenderer.sprite = SpriteNumber01;
+        }
+        else if (elapsedTime > 6 * TimeFactor)
+        {
+            spriteRenderer.sprite = SpriteNumber03;
+            DangerDone = true;
+            InputGiven = false;
+        }
+    }
+}
